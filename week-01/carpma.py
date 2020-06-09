50cfed6a79565fcf522a0070ca5a34e26890d423
Umut Akin <umutakin@fastmail.com>
2020-06-10 00:40:40 +0300

    week-02 - week-01-odevler + week-01-recap

---
+++ week-01/carpma.py
@@ -0,0 +1,8 @@
+print()
+
+x = int(input("x degerini girin: "))
+y = int(input("y degerini girin: "))
+
+print("x * y = ", x * y)
+
+print()
---
+++ week-01/hi.py
@@ -0,0 +1,4 @@
+
+print("")
+print("hello ai era")
+print("")
